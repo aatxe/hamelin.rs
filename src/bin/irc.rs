5f39b3ba309ca9948f6395addde6deb016261c99
Aaron Weiss <aaronweiss74@gmail.com>
2015-02-16 23:01:13 -0500

    IRC daemon is now configured via a config file.

--- src/bin/irc.rs
+++ src/bin/irc.rs
@@ -5,12 +5,11 @@
 
 #[cfg(feature = "irc")] use std::borrow::ToOwned;
 #[cfg(feature = "irc")] use std::collections::HashMap;
-#[cfg(feature = "irc")] use std::default::Default;
 #[cfg(feature = "irc")] use std::env::args;
 #[cfg(feature = "irc")] use std::sync::{Arc, Mutex};
 #[cfg(feature = "irc")] use std::thread::Thread;
 #[cfg(feature = "irc")] use hamelin::{Hamelin, HamelinGuard};
-#[cfg(feature = "irc")] use irc::client::data::{Command, Config};
+#[cfg(feature = "irc")] use irc::client::data::Command;
 #[cfg(feature = "irc")] use irc::client::data::Command::PRIVMSG;
 #[cfg(feature = "irc")] use irc::client::server::{IrcServer, Server};
 #[cfg(feature = "irc")] use irc::client::server::utils::Wrapper;
@@ -18,32 +17,19 @@
 #[cfg(feature = "irc")]
 fn main() {
     let args: Vec<_> = args().skip(1).collect();
-    if args.len() < 4 {
-        println!("Usage: hamelin host port nickname [-ssl] command [args]");
+    if args.len() < 2 {
+        println!("Usage: hamelin config command [args]");
         return;
     }
-    let use_ssl = args[3] == "-ssl";
-    let hamelin = Arc::new(Hamelin::new(
-        if use_ssl { &args[4] } else { &args[3] }, if use_ssl && args.len() > 5 {
-            Some(&args[5..])
-        } else if args.len() > 4 {
-            Some(&args[4..])
-        } else {
-            None
-        }
-    ));    
-    let config = Config {
-        nickname: Some(args[2].to_owned()),
-        server: Some(args[0].to_owned()),
-        port: args[1].parse().ok(),
-        channels: Some(vec!["#pdgn".to_owned()]),
-        use_ssl: if use_ssl { Some(true) } else { None },
-        .. Default::default()
-    };
+    let hamelin = Arc::new(Hamelin::new(&args[1], if args.len() > 2 { 
+        Some(&args[2..]) 
+    } else {
+        None
+    }));   
     let cache: Arc<Mutex<HashMap<String, HamelinGuard>>> = Arc::new(Mutex::new(HashMap::new()));
     loop {
-        let irc_server = Arc::new(IrcServer::from_config(config.clone())
-                                  .ok().expect("Failed to connect to IRC server."));
+        let irc_server = Arc::new(IrcServer::new(&args[0]).unwrap());
+                                  // .ok().expect("Failed to connect to IRC server."));
         let server_ref = irc_server.clone();
         let cache_ref = cache.clone();
         Thread::spawn(move || { 
